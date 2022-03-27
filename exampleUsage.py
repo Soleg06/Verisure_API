ffe27488c4d5f628ad093e847690a37408a910ee
Soleg06 <52281910+Soleg06@users.noreply.github.com>
2022-03-28 00:43:14 +0200

    Update exampleUsage.py

--- exampleUsage.py
+++ exampleUsage.py
@@ -1,56 +1,60 @@
 import verisureGrafqlAPI
 from pprint import pprint
 import arrow
+import time
 
 username = "firstname.lastname@something.com"
 password = "**********"
 
 vs = verisureGrafqlAPI.Verisure()
-#vs.getMfaToken(username, password) this works but using it keeps logging out my iphone app....
+#vs.getMfaToken(False, username, password)
 vs.login(username, password)
 #vs.getAllInstallations()
 
-print("Climate\n")
-pprint(vs.getClimate())
+while True:
+  print("Climate\n")
+  pprint(vs.getClimate())
 
-print("Doorwindow\n")
-pprint(vs.getDoorWindow())
+  print("Doorwindow\n")
+  pprint(vs.getDoorWindow())
 
-print("Armstate\n")
-pprint(vs.getArmState())
+  print("Armstate\n")
+  pprint(vs.getArmState())
 
-print("Broadband\n")
-pprint(vs.getBroadbandStatus())
+  print("Broadband\n")
+  pprint(vs.getBroadbandStatus())
 
-print("Smartplug\n")
-pprint(vs.read_smartplug_state())
+  print("Smartplug\n")
+  pprint(vs.read_smartplug_state())
 
-print("Usertracking\n")
-pprint(vs.userTracking())
+  print("Usertracking\n")
+  pprint(vs.userTracking())
 
-print("Vaccationmode\n")
-pprint(vs.getVacationMode())
+  print("Vaccationmode\n")
+  pprint(vs.getVacationMode())
 
-print("Eventlogcategories\n")
-categories = vs.getEventLogCategories()
-pprint(categories)
+  print("Eventlogcategories\n")
+  categories = vs.getEventLogCategories()
+  pprint(categories)
 
-print("Eventhistory\n")
-#pprint(vs.getEventLog("2022-03-01", "2022-03-04", ['TECHNICAL','FIRE','SOS','INTRUSION','CLIMATE','WARNING','POWERCONTROL','PICTURE','ARM','DISARM','CAMERA_SETTINGS','DOORWINDOW_STATE_OPENED','DOORWINDOW_STATE_CLOSED']))
-pprint(vs.getEventLog(arrow.now().shift(days=-7), arrow.now(), categories))
+  print("Eventhistory\n")
+  #pprint(vs.getEventLog("2022-03-01", "2022-03-04", ['TECHNICAL','FIRE','SOS','INTRUSION','CLIMATE','WARNING','POWERCONTROL','PICTURE','ARM','DISARM','CAMERA_SETTINGS','DOORWINDOW_STATE_OPENED','DOORWINDOW_STATE_CLOSED']))
+  pprint(vs.getEventLog(arrow.now().shift(days=-7), arrow.now(), categories))
 
-print("getCommunications\n")
-pprint(vs.getCommunication())
+  print("getCommunications\n")
+  pprint(vs.getCommunication())
 
-print("getInstallation\n")
-pprint(vs.getInstallation())
+  print("getInstallation\n")
+  pprint(vs.getInstallation())
 
-print("getUsers\n")
-pprint(vs.getUsers())
+  print("getUsers\n")
+  pprint(vs.getUsers())
 
-print("getvacation and petsettings\n")
-pprint(vs.getVacationModeAndPetSetting())
-
+  print("getvacation and petsettings\n")
+  pprint(vs.getVacationModeAndPetSetting())
+  time.sleep(5*60)
+  vs.renewToken() # renew the token using renew token cookie
+  
 print("getCentralUnit")
 pprint(vs.getCentralUnit())
 
